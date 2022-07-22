f8adbf355f780ac1c7f5caab4069dd3cf305af1b
TimzOwen <timzowen@gmail.com>
2022-07-22 10:01:10 +0300

    firebase authenticatoin

--- app/src/main/java/com/timzowen/firenaselogin/LoginActivity.kt
+++ app/src/main/java/com/timzowen/firenaselogin/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.timzowen.idoctor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.timzowen.firenaselogin.MainActivity
 import com.timzowen.firenaselogin.databinding.ActivityLoginBinding
@@ -11,22 +13,44 @@ import com.timzowen.firenaselogin.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginBinding
-    private lateinit var dbRef : DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // make a direct call to dial intents
         binding.btnCallEmergency.setOnClickListener {
             startActivity(Intent(this,EmergencyActivity::class.java))
         }
+        //navigate back to sign up activity
         binding.btnSignUp.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
         }
+
+        //Authenticate user login
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+
+            val userEmail = binding.etSigninEmail.text.toString()
+            val userPassword = binding.etPasswordLogin.text.toString()
+
+            if (userEmail.isNotEmpty() && userPassword.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
--- app/src/main/java/com/timzowen/firenaselogin/SignUpActivity.kt
+++ app/src/main/java/com/timzowen/firenaselogin/SignUpActivity.kt
@@ -4,45 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 import com.timzowen.firenaselogin.databinding.ActivitySignUpBinding
-import com.timzowen.idoctor.data.Users
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignUpBinding
-    private lateinit var dbRef : DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.tvMemberLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         binding.btnCreateUserAccountf.setOnClickListener {
 
-            val fName = binding.etUserNameSignup.text.toString()
-            val age = binding.etAge.text.toString()
-            val phoneNumber =   binding.etPhoneNumberSignup.text.toString()
+            val userEmail = binding.etSignupUserEmail.text.toString()
+            val password = binding.etUsersignupPassword.text.toString()
+            val confirmPswd = binding.etSignupConfirmPswd.text.toString()
 
-            dbRef = FirebaseDatabase.getInstance().getReference("Users")
-            val user = Users(fName,age,phoneNumber)
-            dbRef.child(fName).setValue(user).addOnSuccessListener {
-
-                binding.etUserNameSignup.text.clear()
-                binding.etAge.text.clear()
-                binding.etPhoneNumberSignup.text.clear()
-
-                Toast.makeText(this, "Account created....", Toast.LENGTH_LONG).show()
-
-                startActivity(Intent(this,LoginActivity::class.java))
-
-            }.addOnFailureListener {
-                Toast.makeText(this, "Failed, try again..", Toast.LENGTH_LONG).show()
+            //check if the fields are not empty
+            if (userEmail.isNotEmpty() && password.isNotEmpty() && confirmPswd.isNotEmpty()) {
+                if (password == confirmPswd) {
+                    firebaseAuth.createUserWithEmailAndPassword(userEmail, password)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                // sign up user on success
+                                val intent = Intent(this, LoginActivity::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                } else {
+                    Toast.makeText(this, "field cannot be empty", Toast.LENGTH_SHORT).show()
+                }
             }
-        }
-        binding.tvMemberLogin.setOnClickListener{
-            startActivity(Intent(this,LoginActivity::class.java))
+
         }
 
     }
