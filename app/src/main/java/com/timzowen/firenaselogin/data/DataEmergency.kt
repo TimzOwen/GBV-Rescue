3c096a6b584701e35361713cc3eafe4e7648ddab
TimzOwen <timzowen@gmail.com>
2022-07-25 00:12:29 +0300

    udpated missing data

--- app/src/main/java/com/timzowen/firenaselogin/data/DataEmergency.kt
+++ app/src/main/java/com/timzowen/firenaselogin/data/DataEmergency.kt
@@ -8,20 +8,20 @@ class DataEmergency {
         return listOf(
             EmergencyNumbers("Nairobi County", "Kijabe Hosp", "0702020202"),
             EmergencyNumbers("Nakuru County", "Kabarak Hosp", "0703030303"),
-            EmergencyNumbers("Baringo County", "Mercy Hosp", "0704040404"),
+            EmergencyNumbers("kisumu County", "Mercy Hosp", "0704040404"),
             EmergencyNumbers("Mombasa County", "Mediheal Hosp", "0705050505"),
-            EmergencyNumbers("Nairobi County", "Kijabe", "0706060606"),
-            EmergencyNumbers("Mombasa County", "Kibaki", "0707070707"),
-            EmergencyNumbers("Turkana County", "Mwanga Hosp", "0708080808"),
-            EmergencyNumbers("Bomet County", "Whales Hosp", "0709090909"),
-            EmergencyNumbers("Nairobi County", "Kijabe Hosp", "0710101010"),
-            EmergencyNumbers("Nakuru County", "Kabarak Hosp", "0711111111"),
-            EmergencyNumbers("Baringo County", "Mercy Hosp", "0712121212"),
-            EmergencyNumbers("Mombasa County", "Mediheal Hosp", "0713131313"),
-            EmergencyNumbers("Nairobi County", "Kijabe", "0714141414"),
-            EmergencyNumbers("Mombasa County", "Kibaki", "0715151515"),
-            EmergencyNumbers("Turkana County", "Mwanga Hosp", "071616161"),
-            EmergencyNumbers("Bomet County", "Whales Hosp", "0717171717")
+            EmergencyNumbers("Nakuru County", "PGH Nakuru", "0706060606"),
+            EmergencyNumbers("Nakuru County", "Mogotio SCH", "0707070707"),
+            EmergencyNumbers("Baringo County", "Kabarnet CRH ", "0708080808"),
+            EmergencyNumbers("Nairobi County", "KU CRH ", "0709090909"),
+            EmergencyNumbers("Nairobi County", "Keringet SCH", "0710101010"),
+            EmergencyNumbers("Nakuru County", "Molo Hosp", "0711111111"),
+            EmergencyNumbers("Eldoret County", "Moi Refferal", "0712121212"),
+            EmergencyNumbers("UasinG County", "Eldoret District", "0713131313"),
+            EmergencyNumbers("Nakuru County", "Mirugi SCH", "0714141414"),
+            EmergencyNumbers("Kericho County", "Bliss HOSP", "0715151515"),
+            EmergencyNumbers("Nairobi County", "Nairobi Hosp", "071616161"),
+            EmergencyNumbers("Nairobi County", "Nairobi Womens", "0717171717")
         )
     }
 
