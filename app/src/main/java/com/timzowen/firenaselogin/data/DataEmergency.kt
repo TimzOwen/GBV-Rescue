6f789d8d23d2e2384949f5c43b0410df9ed81303
TimzOwen <timzowen@gmail.com>
2022-07-25 00:03:40 +0300

    updated data package

--- app/src/main/java/com/timzowen/firenaselogin/data/DataEmergency.kt
+++ app/src/main/java/com/timzowen/firenaselogin/data/DataEmergency.kt
@@ -4,16 +4,24 @@ import com.timzowen.idoctor.model.EmergencyNumbers
 
 class DataEmergency {
 
-    fun loadCalls() : List<EmergencyNumbers>{
+    fun loadCalls(): List<EmergencyNumbers> {
         return listOf(
-            EmergencyNumbers("Nairobi County", "Kijabe Hosp","0722000000"),
-            EmergencyNumbers("Nakuru County","Kabarak Hosp","0721000000"),
-            EmergencyNumbers("Baringo County", "Mercy Hosp","0723000000"),
-            EmergencyNumbers("Mombasa County","Mediheal Hosp","0724000000"),
-            EmergencyNumbers("Nairobi County", "Kijabe","0722000000"),
-            EmergencyNumbers("Mombasa County","Kibaki","0721000000"),
-            EmergencyNumbers("Turkana County", "Mwanga Hosp","0723000000"),
-            EmergencyNumbers("Bomet County","Whales Hosp","0724000000")
+            EmergencyNumbers("Nairobi County", "Kijabe Hosp", "0702020202"),
+            EmergencyNumbers("Nakuru County", "Kabarak Hosp", "0703030303"),
+            EmergencyNumbers("Baringo County", "Mercy Hosp", "0704040404"),
+            EmergencyNumbers("Mombasa County", "Mediheal Hosp", "0705050505"),
+            EmergencyNumbers("Nairobi County", "Kijabe", "0706060606"),
+            EmergencyNumbers("Mombasa County", "Kibaki", "0707070707"),
+            EmergencyNumbers("Turkana County", "Mwanga Hosp", "0708080808"),
+            EmergencyNumbers("Bomet County", "Whales Hosp", "0709090909"),
+            EmergencyNumbers("Nairobi County", "Kijabe Hosp", "0710101010"),
+            EmergencyNumbers("Nakuru County", "Kabarak Hosp", "0711111111"),
+            EmergencyNumbers("Baringo County", "Mercy Hosp", "0712121212"),
+            EmergencyNumbers("Mombasa County", "Mediheal Hosp", "0713131313"),
+            EmergencyNumbers("Nairobi County", "Kijabe", "0714141414"),
+            EmergencyNumbers("Mombasa County", "Kibaki", "0715151515"),
+            EmergencyNumbers("Turkana County", "Mwanga Hosp", "071616161"),
+            EmergencyNumbers("Bomet County", "Whales Hosp", "0717171717")
         )
     }
 
